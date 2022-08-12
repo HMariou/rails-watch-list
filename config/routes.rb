684c291d5186748b84306bbd255a50f1ac69efe9
Hugo Mariou <mariou.hugo@gmail.com>
2022-08-12 13:33:46 -0300

    bookmark controller updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :lists
+  resources :lists do
+    resources :bookmarks, only: %i[create new]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :bookmarks, only: :delete
 end
