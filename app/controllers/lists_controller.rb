f2905ee78da72c457224a0cdb9aeaedc12acc561
Hugo Mariou <mariou.hugo@gmail.com>
2022-08-13 00:20:58 -0300

    card css added

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,6 +24,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name, :name)
+    params.require(:list).permit(:name, :photo)
   end
 end
